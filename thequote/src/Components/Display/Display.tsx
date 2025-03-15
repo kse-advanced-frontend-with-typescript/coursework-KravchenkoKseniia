726f656f1a0453a735743ea5c24fa23cc904a54b
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-03-15 13:33:43 +0200

    feat: added header

--- thequote/src/Components/Display/Display.tsx
+++ thequote/src/Components/Display/Display.tsx
@@ -1,12 +1,14 @@
 ﻿import React from 'react';
 import styles from './styles.module.css';
 import {StatusBar} from "../StatusBar/StatusBar";
+import {Header} from "../Header/Header";
 
 export const Display = () => {
     return (
         <div className={styles.display}>
             <div className={styles.phone}>
                 <StatusBar />
+                <Header />
             </div>
         </div>
     );
---
+++ thequote/src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import styles from './styles.module.css';
+
+type HeaderProps = {
+    title?: string;
+}
+
+export const Header : React.FC<HeaderProps> = ({title = 'THE QUOTE'}) => {
+    return (
+        <div className={styles.header}>
+            <p className={styles.title}>{title}</p>
+        </div>
+    );
+};
