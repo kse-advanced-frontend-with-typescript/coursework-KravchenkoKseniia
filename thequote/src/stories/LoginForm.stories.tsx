99e56f9fe02afec60b9ae53b56c1e713e7a0bdc4
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-03-20 13:20:05 +0200

    feat: added login form

---
+++ thequote/src/stories/LoginForm.stories.tsx
@@ -0,0 +1,13 @@
+﻿import type {Meta, StoryObj} from '@storybook/react';
+
+import {LoginForm} from '../Components/LoginForm/LoginForm';
+
+const meta: Meta<typeof LoginForm> = {
+    component: LoginForm,
+    title: 'Example/LoginForm',
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
