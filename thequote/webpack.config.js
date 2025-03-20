f8132c87c0e8d9dce35e3b33a04680cdaa49f6e6
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-03-20 17:43:56 +0200

    feat: added dotenv

--- thequote/webpack.config.js
+++ thequote/webpack.config.js
@@ -1,5 +1,6 @@
 ﻿const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.tsx',
@@ -45,6 +46,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
-        })
+        }),
+        new Dotenv()
     ]
 };
