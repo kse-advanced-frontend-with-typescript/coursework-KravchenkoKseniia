62a7a1effba499a0cdd29998d5d9b09b7db15cb1
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-03-27 10:27:30 +0200

    added Socil Media component (just icons)

---
+++ thequote/src/Components/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import styles from './styles.module.css';
+import {
+    FacebookIcon,
+    LinkedinIcon,
+    RedditIcon,
+    TelegramIcon,
+    ThreadsIcon,
+    TwitterIcon,
+    ViberIcon,
+    WhatsappIcon,
+} from 'react-share';
+
+
+
+export const SocialMedia = () => {
+
+    return (
+        <div className={styles.container}>
+            <FacebookIcon size={32} round={true} />
+            <LinkedinIcon size={32} round={true} />
+            <RedditIcon size={32} round={true} />
+            <TelegramIcon size={32} round={true} />
+            <ThreadsIcon size={32} round={true} />
+            <TwitterIcon size={32} round={true} />
+            <ViberIcon size={32} round={true} />
+            <WhatsappIcon size={32} round={true} />
+        </div>
+    );
+
+};
