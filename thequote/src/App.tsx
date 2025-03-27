9bc5a7951f0ff87d4b29d048e6524d7864c856a1
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-03-27 10:30:16 +0200

    added App component

--- thequote/src/App.tsx
+++ thequote/src/App.tsx
@@ -1,6 +1,5 @@
-﻿import React from "react";
-import styles from './main.css'
-import {Display} from "./Components/Display/Display";
+﻿import React from 'react';
+import {Display} from './Components/Display/Display';
 
 
 export const App = () => {
