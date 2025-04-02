ce993cfa0ada548f4d8b7e4ea0c5f16a9adc0b98
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-04-02 20:52:53 +0300

    feat: add tests for DeleteSavedQuote, GetSavedQuoteInfo, and GetUserInfo functions

---
+++ thequote/src/modules/clients/quote/__tests__/get_saved_quote_info.test.ts
@@ -0,0 +1,45 @@
+﻿import {initQuoteAPI, SavedQuoteItem} from '../index';
+describe('Qet Saved Quote Info', () => {
+    const API_KEY = 'some_key';
+
+    describe('when response is valid', () => {
+        const body : SavedQuoteItem = [
+            {
+                _id: 'some_id',
+                username: 'some_username',
+                quote: 'some_quote',
+                author: 'some_author'
+            }
+        ];
+
+        const mocked_fetch = jest.fn().mockImplementation(() => {
+            return new Response(JSON.stringify(body), {
+                status: 200,
+            });
+        });
+
+        const api = initQuoteAPI(API_KEY, mocked_fetch);
+
+        it('should return a quote info', async () => {
+            const res = await api.GetSavedQuoteInfo('some_quote', 'some_author');
+            expect(res).toEqual(body);
+        });
+    });
+
+    describe('when response is not valid', () => {
+        const emptyBody : SavedQuoteItem = [];
+
+        const mocked_fetch = jest.fn().mockImplementation(() => {
+            return new Response(JSON.stringify(emptyBody), {
+                status: 200,
+            });
+        });
+
+        const api = initQuoteAPI(API_KEY, mocked_fetch);
+
+        it('should throw an error', async () => {
+            await expect(api.GetSavedQuoteInfo('invalid_quote', 'invalid_author')).rejects.toThrow('Quote not found');
+        });
+    });
+
+});
