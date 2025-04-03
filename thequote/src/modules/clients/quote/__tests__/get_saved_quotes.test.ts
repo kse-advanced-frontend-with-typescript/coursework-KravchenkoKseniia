13b3d79dc1e1b32c4ca3ff7c7f22ed18e76fa6ef
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-04-03 11:13:48 +0300

    feat: update GetSavedQuotes test to require user ID and username, and handle user not found error

--- thequote/src/modules/clients/quote/__tests__/get_saved_quotes.test.ts
+++ thequote/src/modules/clients/quote/__tests__/get_saved_quotes.test.ts
@@ -1,53 +1,58 @@
-﻿// import {initQuoteAPI, SavedQuoteItem} from '../index';
-// describe('Qet Saved Quotes', () => {
-//     const API_KEY = 'some_key';
-//
-//     describe('when response is valid', () => {
-//         const body : SavedQuoteItem = [
-//             {
-//                 _id: 'some_id',
-//                 username: 'some_username',
-//                 quote: 'some_quote',
-//                 author: 'some_author'
-//             }
-//         ];
-//
-//         const mocked_fetch = jest.fn().mockImplementation(() => {
-//             return new Response(JSON.stringify(body), {
-//                 status: 200,
-//             });
-//         });
-//
-//         const api = initQuoteAPI(API_KEY, mocked_fetch);
-//
-//         it('should return a quote', async () => {
-//             const res = await api.GetSavedQuotes();
-//             expect(res).toEqual(body);
-//         });
-//     });
-//
-//     describe('when response is not valid', () => {
-//         const body : SavedQuoteItem = [
-//             {
-//                 _id: 'some_id',
-//                 // @ts-expect-error To test the error we are passing a number instead of a string
-//                 username: 4,
-//                 quote: 'some_quote',
-//                 author: 'some_author'
-//             }
-//         ];
-//
-//         const mocked_fetch = jest.fn().mockImplementation(() => {
-//             return new Response(JSON.stringify(body), {
-//                 status: 200,
-//             });
-//         });
-//
-//         const api = initQuoteAPI(API_KEY, mocked_fetch);
-//
-//         it('should throw an error', async () => {
-//             await expect(api.GetSavedQuotes()).rejects.toThrow('Data is not valid: /0/username (Expected string)');
-//         });
-//     });
-//
-// });
+﻿import {initQuoteAPI, SavedQuoteItem} from '../index';
+describe('Qet Saved Quotes', () => {
+    const API_KEY = 'some_key';
+
+    describe('when response is valid', () => {
+        const some_username = 'some_username';
+        const some_id = 'some_user_id';
+
+        const body : SavedQuoteItem = [
+            {
+                _id: 'some_id',
+                username: 'some_username',
+                quote: 'some_quote',
+                author: 'some_author'
+            }
+        ];
+
+        const mocked_fetch = jest.fn().mockImplementation(() => {
+            return new Response(JSON.stringify(body), {
+                status: 200,
+            });
+        });
+
+        const api = initQuoteAPI(API_KEY, mocked_fetch);
+
+        it('should return a quote', async () => {
+            const res = await api.GetSavedQuotes(some_id, some_username);
+            expect(res).toEqual(body);
+        });
+    });
+
+    describe('when response is not valid', () => {
+        const some_id = '';
+        const invalid_username = '1234';
+
+        const body : SavedQuoteItem = [
+            {
+                _id: 'some_id',
+                username: 'some_username',
+                quote: 'some_quote',
+                author: 'some_author'
+            }
+        ];
+
+        const mocked_fetch = jest.fn().mockImplementation(() => {
+            return new Response(JSON.stringify(body), {
+                status: 200,
+            });
+        });
+
+        const api = initQuoteAPI(API_KEY, mocked_fetch);
+
+        it('should throw an error', async () => {
+            await expect(api.GetSavedQuotes(some_id, invalid_username)).rejects.toThrow('User not found');
+        });
+    });
+
+});
