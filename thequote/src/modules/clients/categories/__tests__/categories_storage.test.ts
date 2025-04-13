a0e09475ff121a7afa7cb4d8000ca1adb7699726
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-04-13 12:51:09 +0300

    Add tests for categories and token storage APIs; mock localStorage interactions

---
+++ thequote/src/modules/clients/categories/__tests__/categories_storage.test.ts
@@ -0,0 +1,48 @@
+﻿import {initCategoriesAPI} from '../index';
+import {IconType} from '../../../../Components/IconButton/IconButton';
+
+const mockGetItem = jest.fn();
+const mockSetItem = jest.fn();
+const mockRemoveItem = jest.fn();
+
+Object.defineProperty(window, "localStorage", {
+    value: {
+        getItem: (...args: string[]) => mockGetItem(...args),
+        setItem: (...args: string[]) => mockSetItem(...args),
+        removeItem: (...args: string[]) => mockRemoveItem(...args),
+    },
+});
+
+describe('Categories API: Categories Storage', () => {
+    const SESSION_KEY = 'categories';
+
+    const api = initCategoriesAPI();
+
+    beforeEach(() => {
+        mockSetItem.mockClear();
+        mockSetItem.mockClear();
+    });
+
+    it('should save categories to localStorage', () => {
+        const categories: IconType[] = [
+            "change", "creativity", "life"
+        ];
+
+        api.SaveCategories(categories);
+        expect(mockSetItem).toHaveBeenCalledTimes(1);
+        expect(mockSetItem).toHaveBeenCalledWith(SESSION_KEY, JSON.stringify(categories));
+
+    })
+
+    it('should restore categories from localStorage', () => {
+        api.RestoreCategories();
+        expect(mockGetItem).toHaveBeenCalledTimes(1);
+        expect(mockGetItem).toHaveBeenCalledWith(SESSION_KEY);
+    })
+
+    it('should clean token from localStorage', () => {
+        api.CleanCategories();
+        expect(mockRemoveItem).toHaveBeenCalledTimes(1);
+        expect(mockRemoveItem).toHaveBeenCalledWith(SESSION_KEY);
+    })
+});
