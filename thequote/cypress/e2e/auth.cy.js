cc4ba92bd35512eb907e1fb558faa5bb601c92f5
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-04-16 22:52:06 +0300

    Add Cypress tests for saving and viewing quotes; implement scenarios for successful save and retrieval

--- thequote/cypress/e2e/auth.cy.js
+++ thequote/cypress/e2e/auth.cy.js
@@ -1,6 +1,8 @@
 ﻿// cypress/integration/quotePage.spec.js
 describe('Auth', () => {
     const settingsUrl = 'http://localhost:8080/settings';
+    const baseUrl = 'http://localhost:8080/';
+    const saveUrl = 'http://localhost:8080/save';
     const login = 'admin'
     const password = 'admin'
 
@@ -14,6 +16,37 @@ describe('Auth', () => {
 
             cy.contains('Save', {timeout: 10000}).should('be.visible')
             cy.contains('Logout').should('be.visible')
+        })
+
+        it('should be possible to save quote from main page', () => {
+            cy.visit(settingsUrl)
+            cy.contains('Login').click()
+            cy.get('input').first().type(login)
+            cy.get('input').last().type(password)
+            cy.get('form button').click()
+
+            cy.contains('Save', {timeout: 10000}).should('be.visible')
+            cy.contains('Logout').should('be.visible')
+
+            cy.visit(baseUrl)
+            cy.wait(3000)
+            cy.contains('SAVE').click()
+            cy.get('[class*="success"]', {timeout: 10000}).should('be.visible')
+        })
+
+        it('should be possible to see saved quotes', () => {
+            cy.visit(settingsUrl)
+            cy.contains('Login').click()
+            cy.get('input').first().type(login)
+            cy.get('input').last().type(password)
+            cy.get('form button').click()
+            cy.contains('Save', {timeout: 10000}).should('be.visible')
+
+            cy.visit(saveUrl)
+            cy.wait(3000)
+            cy.contains('SAVED QUOTES').should('be.visible')
+            cy.get('[class*="container"]', {timeout: 10000}).first().should('be.visible')
+
         })
     })
 
