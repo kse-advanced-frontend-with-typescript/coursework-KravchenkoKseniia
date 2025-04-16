35d00ffd2854561b7525e6087f78164ecc9af47e
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-04-16 22:25:09 +0300

    Add Cypress integration tests for authentication; include scenarios for valid and invalid login attempts

---
+++ thequote/cypress/e2e/main.cy.js
@@ -0,0 +1,36 @@
+﻿// cypress/integration/quotePage.spec.js
+describe('Auth', () => {
+    const settingsUrl = 'http://localhost:8080/settings';
+    const login = 'admin'
+    const password = 'admin'
+
+    describe('when the user is authorized', () => {
+        it('Add new Item button should be available', () => {
+            cy.visit(settingsUrl)
+            cy.contains('Login').click()
+            cy.get('input').first().type(login)
+            cy.get('input').last().type(password)
+            cy.get('form button').click()
+
+            cy.contains('Save', {timeout: 10000}).should('be.visible')
+        })
+    })
+
+    describe('when the user uses wrong password', () => {
+        it('Add new Item button should be available', () => {
+            cy.visit(settingsUrl)
+            cy.contains('Login').click()
+            cy.get('input').first().type(login)
+            cy.get('input').last().type('wrong password')
+            cy.get('form button').click()
+
+            cy.on('uncaught:exception', () => {
+                return false
+            })
+
+            cy.get('[class*="error"]', {timeout: 10000}).should('be.visible')
+        })
+    })
+
+})
+
